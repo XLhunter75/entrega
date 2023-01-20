f283cd7c19d564d63b5eb7329f0e746e31562203
Fernando <fgpaolini@gmail.com>
2023-01-20 19:54:29 +0100

    Mejoras de CardViews y Layouts

--- app/src/main/java/com/example/entregaprototipo/Shop/FragmentCategory.java
+++ app/src/main/java/com/example/entregaprototipo/Shop/FragmentCategory.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 
 import com.example.entregaprototipo.Adapters.AdpShop;
 import com.example.entregaprototipo.ProductModel.ProductData;
@@ -59,7 +61,7 @@ public class FragmentCategory extends Fragment {
     public void createRecycleProductsA(@NonNull View v, ArrayList<ProductData> list_show){
         AdpShop adpShop_adaptor = new AdpShop(v.getContext(), list_show,true,false);
         RecyclerView recyclerViewMain = v.findViewById(R.id.recicleViewProducts2);
-        recyclerViewMain.setLayoutManager(new LinearLayoutManager(v.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerViewMain.setLayoutManager(new GridLayoutManager(v.getContext(), 2));
         recyclerViewMain.setAdapter(adpShop_adaptor);
     }
 
